fec8b8a972eb476beda15faa0bde20c816f54fe8
Rene <renesuar99@gmail.com>
2022-06-12 19:15:00 -0400

    Changed style of resize button and default size of block grid.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // test variable decleration
-let blockSize = 32;
+let blockSize = 4;
 const MAX_SIZE = 816;
 let testSize =0;
 
 
-
+//creation container variable.
 const container = document.querySelector('#container');
 
 
@@ -22,27 +22,21 @@ container.appendChild(content);
 
 
 
-
+//creation of reszie button.
 const resizeButton = document.querySelector('#resizeButton');
 
 
 
-// let newBlock = 0;
-
-// newBlock = MAX_SIZE/blockSize;
-// let blocksToCreate = blockSize*blockSize; 
-
 
 function createBlocks(blockSize) { 
 
-// for loop to create elements.
+// for loop to create the pixel blocks for etch a sketch
 for (i=0; i < blockSize*blockSize; i++)
 {
 
 let newBlock = 0;
-
+//creating new div block elements which will be drawn on.
 newBlock = MAX_SIZE/blockSize;
-// let blocksToCreate = blockSize*blockSize; 
 
 
     let pixelBlock = document.createElement('div')
@@ -58,16 +52,16 @@ newBlock = MAX_SIZE/blockSize;
 
 }
 
-
+//creating blocks.
 createBlocks(blockSize);
 
-
+// creating array to house all of the pixelBlock divs
 let pixelBlocks = document.querySelectorAll(".pixelBlock");
 
 
-
+// function that allows for coloring the white blocks.
 function colorAdder () {
-
+// loops through and adds event to all pixel blocks.
 for (let pixelBlock of pixelBlocks){
 pixelBlock.addEventListener ('click' && 'mousemove', function(e){
 pixelBlock.style.backgroundColor = "black";
@@ -76,9 +70,14 @@ pixelBlock.style.backgroundColor = "black";
 }
 
 }
+
+// calling function.
 colorAdder ();
 
 
+
+
+// Deletes current art and resizes to new value. 
 
 resizeButton.addEventListener('click', function(e){
 
@@ -86,7 +85,7 @@ blockSize = prompt("Enter how large you want the blocks.")
 
 
 
-
+// removes all pixelBlocks.
 for  (let i =0; i < pixelBlocks.length; i++)  {
 content.removeChild(pixelBlocks[i]);
 
@@ -94,7 +93,12 @@ content.removeChild(pixelBlocks[i]);
 
 
 }
+//creates new blocks to new size and adds back functionality.
 createBlocks(blockSize);
  pixelBlocks = document.querySelectorAll(".pixelBlock");
 colorAdder();
 });
+
+
+
+
